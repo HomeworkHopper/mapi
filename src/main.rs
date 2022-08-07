b9f9c396438915219a814223013da829d162f71a
Shaun Thornton <homeworkhopper@gmail.com>
2022-08-06 23:48:12 -0400

    dotenv setup

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use dotenv;
+
+fn main() {
+    let email = dotenv::var("EMAIL").unwrap();
+    let password = dotenv::var("PASSWORD").unwrap();
+
+    println!("Email: {}, Password: {}", email, password);
+}
